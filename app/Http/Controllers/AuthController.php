0f434bef76e7c3495211bea00b571765c03a1ca6
ikromr04 <ikromr04@gmail.com>
2023-12-27 13:11:00 +0500

    Changes auth check method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
       return response(['message' => 'Вы не авторизованы.'], 401);
     }
 
-    return response(['message' => 'Вы авторизованы.'], 200);
+    return response(request()->user('sanctum'), 200);
   }
 
   public function login(LoginRequest $request)
