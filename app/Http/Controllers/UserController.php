1a42a341ac955161cda169dc147d4549f4aba474
ikromr04 <ikromr04@gmail.com>
2024-01-02 18:56:48 +0500

    Enables employees avatar api routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,25 +29,6 @@ class UserController extends Controller
     return response($user, 200);
   }
 
-  public function store()
-  {
-    // $fields = $request->validate([
-      // 'name' => 'required|string',
-      // 'login' => 'required|unique:users,login',
-      // 'password' => 'required|string|confirmed',
-    // ]);
-
-    // $user = User::create([
-    //   'name' => $fields['name'],
-    //   'login' => $fields['login'],
-    //   'password' => bcrypt($fields['password']),
-    // ]);
-
-    // $user->token = $user->createToken('token')->plainTextToken;
-
-    // return $user;
-  }
-
   public function update(EmployeeUpdateRequest $request, $id)
   {
     $user = User::withoutGlobalScopes()->find($id);
@@ -71,64 +52,7 @@ class UserController extends Controller
     $request->has('positions') && $user->positions()->sync($request->positions);
 
     return User::withDetails()->find($id);
-    // $user = User::find($employeeId);
-
-    // if (request('login') != $user->login) {
-    //   request()->validate([
-    //     'login' => 'required|unique:users,login',
-    //   ]);
-    //   $user->update(['login' => request('login')]);
-    // }
-
-    // $user->update([
-    //   'name' => request('name'),
-    //   'surname' => request('surname'),
-    //   'patronymic' => request('patronymic'),
-    //   'started_work_at' => request('started_work_at'),
-    // ]);
-
-    // $user->jobs()->sync(request('jobs'));
-    // $user->positions()->sync(request('positions'));
-
-    // $user = User::with('jobs', 'positions', 'languages')->find($employeeId);
-
-    // return $user;
   }
-
-  // public function updatePersonalData($employeeId)
-  // {
-  //   $personalData = PersonalData::where('user_id', $employeeId)->first();
-  //   if ($personalData) {
-  //     $personalData->update([
-  //       'birth_date' => request('birth_date'),
-  //       'gender' => request('gender'),
-  //       'nationality' => request('nationality'),
-  //       'citizenship' => request('citizenship'),
-  //       'address' => request('address'),
-  //       'email' => request('email'),
-  //       'tel_1' => request('tel_1'),
-  //       'tel_2' => request('tel_2'),
-  //       'family_status' => request('family_status'),
-  //       'children' => request('children'),
-  //     ]);
-  //     return $personalData;
-  //   }
-  //   $personalData = PersonalData::create([
-  //     'user_id' => $employeeId,
-  //     'birth_date' => request('birth_date'),
-  //     'gender' => request('gender'),
-  //     'nationality' => request('nationality'),
-  //     'citizenship' => request('citizenship'),
-  //     'address' => request('address'),
-  //     'email' => request('email'),
-  //     'tel_1' => request('tel_1'),
-  //     'tel_2' => request('tel_2'),
-  //     'family_status' => request('family_status'),
-  //     'children' => request('children'),
-  //   ]);
-
-  //   return $personalData;
-  // }
 
   public function updateAvatar($id)
   {
@@ -143,9 +67,7 @@ class UserController extends Controller
     $file->move(public_path('/uploads/img/employees'), $fileName);
     $avatar = '/uploads/img/employees/' . $fileName;
 
-    DB::table('users')->where('id', $id)->update([
-      'avatar' => $avatar
-    ]);
+    DB::table('users')->where('id', $id)->update(['avatar' => $avatar]);
 
     return asset($avatar);
   }
@@ -161,67 +83,5 @@ class UserController extends Controller
     DB::table('users')->where('id', $id)->update(['avatar' => null ]);
 
     return '';
-  }
-
-  public function educations($employeeId)
-  {
-    return Education::where('user_id', $employeeId)->get();
-  }
-
-  public function activities($employeeId)
-  {
-    return LaborActivity::where('user_id', $employeeId)->get();
-  }
-
-  public function storeEducation($employeeId)
-  {
-    request()->validate([
-      'started_at' => 'required',
-      'graduated_at' => 'required',
-      'institution' => 'required',
-      'faculty' => 'required',
-      'form' => 'required',
-      'speciality' => 'required',
-    ]);
-
-    return Education::create([
-      'user_id' => $employeeId,
-      'started_at' => request('started_at'),
-      'graduated_at' => request('graduated_at'),
-      'institution' => request('institution'),
-      'faculty' => request('faculty'),
-      'form' => request('form'),
-      'speciality' => request('speciality'),
-    ]);
-  }
-
-  public function storeActivity($employeeId)
-  {
-    request()->validate([
-      'hired_at' => 'required',
-      'dismissed_at' => 'required',
-      'organization' => 'required',
-      'job' => 'required',
-    ]);
-
-    return LaborActivity::create([
-      'user_id' => $employeeId,
-      'hired_at' => request('hired_at'),
-      'dismissed_at' => request('dismissed_at'),
-      'organization' => request('organization'),
-      'job' => request('job'),
-    ]);
-  }
-
-  public function updateLanguages($id)
-  {
-    $user = User::find($id);
-    $languages = [];
-    foreach (request('languages') as $language) {
-      $languages[$language['id']] = ['level' => $language['level']];
-    }
-    $user->languages()->sync($languages);
-
-    return User::detailed()->find($id);
   }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\EducationController;
 use App\Http\Controllers\JobController;
-use App\Http\Controllers\LaborActivityController;
-use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\PositionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -27,29 +24,10 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
   Route::get('/employees', [UserController::class, 'index']);
   Route::get('/employees/{id}', [UserController::class, 'show']);
   Route::put('/employees/{id}', [UserController::class, 'update']);
-
-  // Route::put('/employees', [UserController::class, 'update']);
-  // Route::put('/employees/{id}/avatar', [UserController::class, 'updateAvatar']);
-  // Route::delete('/employees/{id}/avatar', [UserController::class, 'deleteAvatar']);
-  // Route::put('/employees/{id}/languages', [UserController::class, 'updateLanguages']);
-
-  // Route::get('/employees/{employeeId}/personal', [UserController::class, 'personalData']);
-  // Route::put('/employees/{employeeId}/personal', [UserController::class, 'updatePersonalData']);
-  // Route::get('/employees/{employeeId}/educations', [UserController::class, 'educations']);
-  // Route::post('/employees/{employeeId}/educations', [UserController::class, 'storeEducation']);
-  // Route::get('/employees/{employeeId}/activities', [UserController::class, 'activities']);
-  // Route::post('/employees/{employeeId}/activities', [UserController::class, 'storeActivity']);
-
-  // Route::put('/educations/{educationId}', [EducationController::class, 'update']);
-  // Route::delete('/educations/{educationId}', [EducationController::class, 'delete']);
-
-  // Route::put('/labor-activities/{activityId}', [LaborActivityController::class, 'update']);
-  // Route::delete('/labor-activities/{activityId}', [LaborActivityController::class, 'delete']);
+  Route::put('/employees/{id}/avatar', [UserController::class, 'updateAvatar']);
+  Route::delete('/employees/{id}/avatar', [UserController::class, 'deleteAvatar']);
 
   Route::get('/jobs', [JobController::class, 'index']);
 
   Route::get('/positions', [PositionController::class, 'index']);
-
-  // Route::get('/languages', [LanguageController::class, 'index']);
-
 });
