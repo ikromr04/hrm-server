14d03a40f58903fe0a8d31a355467537b4f0f549
ikromr04 <ikromr04@gmail.com>
2024-02-23 14:08:27 +0500

    Makes DepartmentController's store method

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
 <body>
   <noscript>You need to enable JavaScript to run this app.</noscript>
   <div id="root"></div>
-  
-  <script type="module" src="{{ asset('js/index-kAGcuToF.js') }}"></script>
+
+  <script type="module" src="{{ asset('js/index-rJGJ_E3n.js') }}"></script>
 </body>
 </html>
