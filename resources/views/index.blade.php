e95d949e1dbbf377e25dd75a9fbabb87b27bac0c
ikromr04 <ikromr04@gmail.com>
2024-02-28 12:00:43 +0500

    Adds DatabaseSeeder.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="robots" content="noindex, nofollow">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
 
-  <link rel="icon" type="image/svg+xml" href="/evolet.svg" />
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('css/index-g6tG_dQ0.css') }}">
+    <link rel="icon" type="image/svg+xml" href="/evolet.svg" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/index-g6tG_dQ0.css') }}">
 
-  <title>HRM система Эволет</title>
-</head>
-<body>
-  <noscript>You need to enable JavaScript to run this app.</noscript>
-  <div id="root"></div>
+    <title>HRM система Эволет</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
 
-  <script type="module" src="{{ asset('js/index-b5kh-UoB.js') }}"></script>
-</body>
+    <script type="module" src="{{ asset('js/index-b5kh-UoB.js') }}"></script>
+  </body>
 </html>
